687d8c97122b417259b749f6d514efbc36f888d6
RIZKI PRIMA PUTRA <elesya420@gmail.com>
2022-06-22 09:03:31 +0700

    first commit

---
+++ app/src/main/java/com/example/uas_dpm_rizkiprimaputra_2011500006/DataKampus.kt
@@ -0,0 +1,11 @@
+package com.example.uas_dpm_rizkiprimaputra_2011500006
+
+class DataKampus (
+    var NIDN: String,
+    var NmDosen: String,
+    var Jabatan: String,
+    var Golongan: String,
+    var BidangKeahlian: String,
+    var ProgramStudi: String,
+    var PendidikanTerakhir: String
+)
